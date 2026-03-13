8292006a1cd7205a3cc2bdcb06b79604f7335583
Hugo Coto Florez <hugocoto100305@gmail.com>
2026-03-13 10:32:43 +0100

    Updating while he is talking about js

---
+++ base64/base64.h
@@ -0,0 +1,20 @@
+#ifndef B64_H_
+#define B64_H_
+
+int b64_enc_fd(int infd, int outfd, unsigned long size);
+int b64_dec_fd(int infd, int outfd, unsigned long size);
+
+#endif // !64_H_
+
+#ifdef INCLUDE_B64_IMPLEMENTATION
+
+const char b64_lookup[] = {
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+        'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b',
+        'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+        'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3',
+        '4', '5', '6', '7', '8', '9', '+', '/'
+};
+
+
+#endif // !INCLUDE_B64_IMPLEMENTATION
