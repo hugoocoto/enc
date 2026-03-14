500c1157d0f5c93ab8634ec83f34a577564a70cd
Hugo Coto Florez <hugocoto100305@gmail.com>
2026-03-14 01:53:35 +0100

    Adding license to file

--- base64/base64.h
+++ base64/base64.h
@@ -1,3 +1,12 @@
+/* * Copyright (c) 2026 Hugo Coto Florez
+ *
+ * This work is licensed under the Creative Commons Attribution 4.0
+ * International License. To view a copy of this license, visit
+ * http://creativecommons.org/licenses/by/4.0/
+ *
+ * SPDX-License-Identifier: CC-BY-4.0
+ */
+
 #ifndef B64_H_
 #define B64_H_
 
